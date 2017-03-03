7c748e873d51f1eb1692060fda2acc602a1e7138
OmarSwidan <omar.swidan@student.guc.edu.eg>
2017-03-03 23:53:29 +0200

    se

---
+++ SwidanProject/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://omarswidan:Swidan1996@ds157539.mlab.com:57539/omarproject' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
